40969da5bffe5896f27a5bc709bf2f0c1fa3f4ad
RyanL Tower <ryanlittlec@gmail.com>
2023-03-29 17:37:24 -0500

    Established bingo-card-generator

---
+++ bingo-card-generator/__init__.py
@@ -0,0 +1,3 @@
+"""__init__.py: Exposes main function of bingo-card-generator"""
+
+from .main import main as bingo_card_generator_main
---
+++ bingo-card-generator/main.py
@@ -0,0 +1,11 @@
+"""main.py: Main file for bingo-card-generator"""
+
+
+def main():
+    """Bingo Card Generator main function"""
+    print("Hello world")
+    return
+
+
+if __name__ == "__main__":
+    main()
