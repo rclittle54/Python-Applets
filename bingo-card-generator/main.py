e2ec920ba00705987ba0b326bdb14c3fbbd9255e
RyanL Tower <ryanlittlec@gmail.com>
2023-03-29 17:46:17 -0500

    Create example input, and a function to parse input txt files

--- bingo-card-generator/main.py
+++ bingo-card-generator/main.py
@@ -1,9 +1,34 @@
 """main.py: Main file for bingo-card-generator"""
 
+# Imports
+import pandas as pd
+import os
+
+_EXPECTEDVALS = 24
+
+def parseTextFileInput(textFile: str) -> list:
+    """Return a list of text elements from the provided text file"""
+
+    output = None
+
+    # Parse the file
+    if os.path.isfile(textFile):
+        with open(textFile, 'r') as f:
+            output = [line.replace("\n", "") for line in f]
+
+    # Handle funkiness
+    if output:
+        assert len(output) == _EXPECTEDVALS, "Parsed txt file did not have expected values"
+    else:
+        print("Warning: provided text file was unable to be found")
+
+    return output
+
 
 def main():
     """Bingo Card Generator main function"""
     print("Hello world")
+    print(parseTextFileInput("bingo-card-generator/exampleInput.txt"))
     return
 
 
